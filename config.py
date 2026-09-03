b02a40c623adebd0824e3ecec3540c4e60301b2f
Sathvara-Amitkumar <sathvaraamit2005@gmail.com>
2026-09-03 10:37:20 +0530

    fix the configuration file

--- config.py
+++ config.py
@@ -1,6 +1,6 @@
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy import create_engine
 
-db_url = "postgresql+psycopg2://postgres:123@localhost:2580/project_fastapi"
+db_url = "postgresql+psycopg2://postgres:123@localhost:5432/project_fastapi"
 engine = create_engine(db_url)
 session = sessionmaker(autocommit=False, autoflush=False, bind=engine)
